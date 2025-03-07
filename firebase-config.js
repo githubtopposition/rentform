d43bec28f67b365767378035f1b60b64d5a60639
githubtopposition <126532056+githubtopposition@users.noreply.github.com>
2025-03-07 02:36:35 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,14 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
 
+// Замените на ваши реальные ключи
 const firebaseConfig = {
-  apiKey: "AIzaSyAt5epMOb...",
+  apiKey: "AIzaSy...",
   authDomain: "rent-form.firebaseapp.com",
   projectId: "rent-form",
   storageBucket: "rent-form.appspot.com",
-  messagingSenderId: "136805752728",
-  appId: "1:136805752728:web:0d9a1bdb1b65fda0aafded"
+  messagingSenderId: "1234567890",
+  appId: "1:1234567890:web:abc123"
 };
 
 const app = initializeApp(firebaseConfig);
