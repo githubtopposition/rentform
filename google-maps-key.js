8a1cb0b08f47c3d1bd8149dad5f53946d2617ddf
githubtopposition <126532056+githubtopposition@users.noreply.github.com>
2025-03-12 02:40:30 -0400

    Create google-maps-key.js

---
+++ google-maps-key.js
@@ -0,0 +1,5 @@
+// google-maps-key.js
+
+// Ваш ключ (не забудьте, что, если этот файл попадёт в публичный репозиторий,
+// пользователь всё равно увидит ключ в DevTools. Но обычно для frontend это нормально.)
+export const GOOGLE_MAPS_KEY = "AIzaSyBaGi8plb8PBaCyKHdmEDye5WamGrlLgg8";
